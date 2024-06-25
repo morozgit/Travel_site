7fdc27c117b51e6daef6550fc0f9681508d039e2
morozgit <69896562+morozgit@users.noreply.github.com>
2024-06-25 23:42:08 +0300

    update ci

--- backend/src/db.py
+++ backend/src/db.py
@@ -7,7 +7,6 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-print(DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 metadata = MetaData()
